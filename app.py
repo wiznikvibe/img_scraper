02127ed66f89c0e9f627394a11cbb5d5a8dbe1d2
wiznikvibe <nikhilshetty439@gmail.com>
2023-08-01 19:31:48 +0530

    Image Scraper

--- app.py
+++ app.py
@@ -10,35 +10,49 @@ from bs4 import BeautifulSoup
 app = Flask(__name__)
 logging.basicConfig(filename="./logs/scraper_logs.log",level=logging.INFO, format='%(levelname)s %(asctime)s %(name)s: %(message)s')
 
-@app.route("/")
-def home():
-    # logging.info("Home Page Called")
-    app.logger.info("App Running")
-    return render_template("home.html")
-
-@app.route("/search", methods=['GET','POST'])
+@app.route("/", methods=['GET','POST'])
 def search():
-    if request.method == 'GET':
-        app.logger.info("Search Initiated")
-        return render_template('search.html', title='Search')
+    
+    if request.method == 'POST':
+        try:
+            # Creating new folder inside the directory
+            save_directory = "images/"
+            if not os.path.exists(save_directory):
+                os.makedirs(save_dirs)
+            
+
+            
+            query = request.form['query'].replace(" ", "")
+            header = {"User-Agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/58.0.3029.110 Safari/537.36"}
+            response = requests.get(f"https://www.google.com/search?sxsrf=AB5stBj-yjpQW68M7EDDc06zYGXn3hCBGw:1690896467683&q={query}&tbm=isch&source=lnms&sa=X&ved=2ahUKEwj7mruayLuAAxXPxDgGHYYUAj8Q0pQJegQIDRAB&biw=767&bih=708&dpr=1.25", headers=header)
+            
+            # Storing the response content 
+            content = BeautifulSoup(response.content, "html.parser")
+
+            image_tags = content.findAll("img")
+            del image_tags[0]
+            img_data = []
+            for index, image_tag in enumerate(image_tags):
+                image_url = image_tag['src']
+                image_data = requests.get(image_url).content
+                
+                mydict = {"Index":index, "Image":image_data}
+
+                img_data.append(mydict)
+                with open(os.path.join(save_directory,f"{query}_{image_tag.index}.jpg"), 'wb') as f:
+                    f.write(image_data)
+            client = pymongo.MongoClient("mongodb+srv://wiznikvibe:3RfTsmlZz7ncmxpt@cluster0.1dfdb77.mongodb.net/?retryWrites=true&w=majority")
+            db = client['images']
+            review_col = db['image_data']
+            review_col.insert_many(img_data)
+
+            return "Images Loaded"
+        except Exception as e:
+            logging.info(e)
+            return "Something went wrong"
+
     else:
-        # Creating new folder inside the directory
-        save_dirs = "images/"
-        if not os.path.exists(save_dirs):
-            os.makedirs(save_dirs)
-        
-        query = request.form['query'].replace(" ", "")
-        response = requests.get(f"https://www.google.com/search?q={query}&tbm=isch&ved=2ahUKEwiNkKj3y7WAAxVE_DgGHcCQA2AQ2-cCegQIABAA&oq=ronoroa+&gs_lcp=CgNpbWcQARgAMgcIABCKBRBDMgUIABCABDIFCAAQgAQyBQgAEIAEMgcIABCKBRBDMgUIABCABDIFCAAQgAQyBAgAEB4yBggAEAUQHjIJCAAQGBCABBAKOgQIIxAnOg0IABCKBRCxAxCDARBDOggIABCABBCxAzoKCAAQigUQsQMQQ1D1CFiHFGC-HWgAcAB4AIABowGIAcIIkgEDMC45mAEAoAEBqgELZ3dzLXdpei1pbWfAAQE&sclient=img&ei=7ebFZM3lEMT44-EPwKGOgAY&bih=718&biw=1536")
-        
-        # Storing the response content 
-        content = BeautifulSoup(response.content, "html.parser")
-
-        image_tags = content.findAll("img")
-
-
-
-        return render_template('result.html', files=image_tags)
-
+        return render_template('index.html')
 
 
 if __name__ == "__main__":
