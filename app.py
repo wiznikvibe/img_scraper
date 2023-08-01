ec742d75582152db4b30bbade86dc11237dcf092
wiznikvibe <nikhilshetty439@gmail.com>
2023-08-01 18:38:11 +0530

    Reading the Result

--- app.py
+++ app.py
@@ -22,8 +22,22 @@ def search():
         app.logger.info("Search Initiated")
         return render_template('search.html', title='Search')
     else:
+        # Creating new folder inside the directory
+        save_dirs = "images/"
+        if not os.path.exists(save_dirs):
+            os.makedirs(save_dirs)
+        
         query = request.form['query'].replace(" ", "")
-        return render_template('result.html', query=query)
+        response = requests.get(f"https://www.google.com/search?q={query}&tbm=isch&ved=2ahUKEwiNkKj3y7WAAxVE_DgGHcCQA2AQ2-cCegQIABAA&oq=ronoroa+&gs_lcp=CgNpbWcQARgAMgcIABCKBRBDMgUIABCABDIFCAAQgAQyBQgAEIAEMgcIABCKBRBDMgUIABCABDIFCAAQgAQyBAgAEB4yBggAEAUQHjIJCAAQGBCABBAKOgQIIxAnOg0IABCKBRCxAxCDARBDOggIABCABBCxAzoKCAAQigUQsQMQQ1D1CFiHFGC-HWgAcAB4AIABowGIAcIIkgEDMC45mAEAoAEBqgELZ3dzLXdpei1pbWfAAQE&sclient=img&ei=7ebFZM3lEMT44-EPwKGOgAY&bih=718&biw=1536")
+        
+        # Storing the response content 
+        content = BeautifulSoup(response.content, "html.parser")
+
+        image_tags = content.findAll("img")
+
+
+
+        return render_template('result.html', files=image_tags)
 
 
 
